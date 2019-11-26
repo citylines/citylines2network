df8a4db68b6b12703478d996ec3882a2c8e21843
Bruno Salerno <br.salerno@gmail.com>
2019-11-26 22:54:07 +0100

    Add better API

---
+++ citylines2network.py
@@ -0,0 +1,34 @@
+from lib.system import System
+
+class Citylines2Network:
+    def __init__(self, edges, nodes):
+        self._systems_dict = self._build_systems_dict(edges, nodes)
+
+    def _build_systems_dict(self, edges, nodes):
+        sdict = {}
+
+        features_list = [edges, nodes]
+        keys = ['edges','nodes']
+        for i in range(2):
+            features = features_list[i]
+            key = keys[i]
+            for f in features:
+                for l in f['properties']['lines']:
+                    system = l['system']
+                    line = l['line']
+                    if not system in sdict:
+                        sdict[system] = {}
+                    if not line in sdict[system]:
+                        sdict[system][line] = {'edges':[], 'nodes':[]}
+                    sdict[system][line][key].append(f)
+
+        return sdict
+
+    def system_names(self):
+        return list(self._systems_dict.keys())
+
+    def system_line_names(self, name):
+        return list(self._systems_dict[name].keys())
+
+    def system(self, name, year, lines=[]):
+        return System(self._systems_dict[name], year=year, line_names=lines)
--- main.py
+++ main.py
@@ -8,52 +8,33 @@ matplotlib.use('TkAgg')
 
 import matplotlib.pyplot as plt
 import networkx
-
-from lib.system import System
-
-def build_systems_dict(edges, nodes):
-    sdict = {}
-
-    features_list = [edges, nodes]
-    keys = ['edges','nodes']
-    for i in range(2):
-        features = features_list[i]
-        key = keys[i]
-        for f in features:
-            for l in f['properties']['lines']:
-                system = l['system']
-                line = l['line']
-                if not system in sdict:
-                    sdict[system] = {}
-                if not line in sdict[system]:
-                    sdict[system][line] = {'edges':[], 'nodes':[]}
-                sdict[system][line][key].append(f)
-
-    return sdict
+from citylines2network import Citylines2Network
 
 def load(edges_fc, nodes_fc):
-    sdict = build_systems_dict(edges_fc['features'], nodes_fc['features'])
+    c2n = Citylines2Network(edges_fc['features'], nodes_fc['features'])
+    print(c2n.system_names())
+    print(c2n.system_line_names('Sistema de Transporte Colectivo (STC)'))
 
-    subte = System(sdict['Subte'], year=1996)
+    metro = c2n.system('Sistema de Transporte Colectivo (STC)', year=1996)
 
-    pos = dict([(node['properties']['id'],list(node['geometry'].coords)[0]) for node in subte.nodes])
-    networkx.draw(subte.graph(),pos,with_labels=True,node_size=50, font_size=9)
+    pos = dict([(node['properties']['id'],list(node['geometry'].coords)[0]) for node in metro.nodes])
+    networkx.draw(metro.graph(),pos,with_labels=True,node_size=50, font_size=9)
 
     fig,ax = plt.subplots(1,1,sharex=True,sharey=True)
 
-    gpd.GeoDataFrame(geometry=subte.routes).plot(ax=ax)
-    geometries = [node['geometry'] for node in subte.nodes]
+    gpd.GeoDataFrame(geometry=metro.routes).plot(ax=ax)
+    geometries = [node['geometry'] for node in metro.nodes]
     gpd.GeoDataFrame(geometry=geometries).plot(ax=ax, color='red')
 
-    for i in range(len(subte.nodes)):
-        coords = list(subte.nodes[i]['geometry'].coords)[0]
+    for i in range(len(metro.nodes)):
+        coords = list(metro.nodes[i]['geometry'].coords)[0]
         plt.annotate(str(i), xy=coords)
 
     plt.show()
 
 if __name__ == '__main__':
-    edges_filename = 'data/buenos-aires_sections.geojson'
-    nodes_filename = 'data/buenos-aires_stations.geojson'
+    edges_filename = 'data/mexico-city_sections.geojson'
+    nodes_filename = 'data/mexico-city_stations.geojson'
 
     with open(edges_filename) as edges_file, open(nodes_filename) as nodes_file:
         load(json.load(edges_file), json.load(nodes_file))
