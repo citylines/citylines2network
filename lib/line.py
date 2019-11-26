fad613a07f6b14d3c17102d4127ba1e3f1ea979d
Bruno Salerno <br.salerno@gmail.com>
2019-11-26 11:24:52 +0100

    Add Line class

---
+++ lib/line.py
@@ -0,0 +1,70 @@
+from shapely import geometry, ops
+import networkx
+
+class Line:
+    def __init__(self, edges_fc, nodes_fc):
+        self.route = self._join_features(edges_fc)
+        nodes = self._snap_nodes(nodes_fc)
+
+        self.nodes = self._sort_nodes(nodes)
+        self.edges = self._build_edges()
+
+    def _join_features(self, data):
+        edges = [geometry.shape(f['geometry']) for f in data['features']]
+
+        multiline = ops.linemerge(edges)
+        coords_list = [list(line.coords) for line in multiline]
+
+        return geometry.LineString([item for sublist in coords_list for item in sublist])
+
+
+    def _snap_nodes(self, nodes_collection):
+        nodes = []
+        for node in nodes_collection['features']:
+            p = geometry.shape(node['geometry'])
+            projected_p = self.route.interpolate(self.route.project(p))
+            new_node = {
+                    'properties': node['properties'],
+                    'geometry': projected_p
+                    }
+            nodes.append(new_node)
+        return nodes
+
+    def _sort_nodes(self, nodes):
+        coords = self.route.coords
+        sorted_nodes = []
+        for i in range(2, len(coords)):
+            # We only take the segment formed by 2 coords
+            l = geometry.LineString(coords[i-2:i])
+
+            nearest = None
+            nearest_index = None
+            for i in range(len(nodes)):
+                n = nodes[i]
+                if l.distance(n['geometry']) < 1e-8:
+                    nearest = n
+                    nearest_index = i
+
+            if nearest:
+                del(nodes[nearest_index])
+                sorted_nodes.append(nearest)
+        return sorted_nodes
+
+    def _build_edges(self):
+        nodes_geom = geometry.MultiPoint([node['geometry'] for node in self.nodes])
+        return ops.split(self.route, nodes_geom)
+
+    def graph(self):
+        network = networkx.Graph()
+
+        for i in range(0, len(self.nodes)):
+            node = self.nodes[i]
+            node_id = node['properties']['id']
+            network.add_node(node_id)
+            if i > 0:
+                previous_node_id = self.nodes[i-1]['properties']['id']
+                network.add_edge(previous_node_id, node_id)
+
+        return network
+
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-from shapely import geometry, ops
 import geopandas as gpd
 
 import matplotlib
@@ -8,80 +7,24 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import networkx
 
-def join_features(data):
-    edges = [geometry.shape(f['geometry']) for f in data['features']]
-
-    multiline = ops.linemerge(edges)
-    coords_list = [list(line.coords) for line in multiline]
-
-    return geometry.LineString([item for sublist in coords_list for item in sublist])
-
-
-def snap_nodes(line, nodes_collection):
-    nodes = []
-    for node in nodes_collection['features']:
-        p = geometry.shape(node['geometry'])
-        projected_p = line.interpolate(line.project(p))
-        new_node = {
-                'properties': node['properties'],
-                'geometry': projected_p
-                }
-        nodes.append(new_node)
-    return nodes
-
-def sort_nodes(line, nodes):
-    coords = line.coords
-    sorted_nodes = []
-    for i in range(2, len(coords)):
-        # We only take the segment formed by 2 coords
-        l = geometry.LineString(coords[i-2:i])
-
-        nearest = None
-        nearest_index = None
-        for i in range(len(nodes)):
-            n = nodes[i]
-            if l.distance(n['geometry']) < 1e-8:
-                nearest = n
-                nearest_index = i
-
-        if nearest:
-            del(nodes[nearest_index])
-            sorted_nodes.append(nearest)
-    return sorted_nodes
-
-def build_graph(line, nodes):
-    network = networkx.Graph()
-
-    nodes_geom = geometry.MultiPoint([node['geometry'] for node in nodes])
-    edges = ops.split(line, nodes_geom)
-
-    for i in range(0, len(nodes)):
-        node = nodes[i]
-        node_id = node['properties']['id']
-        network.add_node(node_id)
-        if i > 0:
-            previous_node_id = nodes[i-1]['properties']['id']
-            network.add_edge(previous_node_id, node_id)
-
-    return network
+from lib.line import Line
 
 def load(edges_fc, nodes_fc):
-    line = join_features(edges_fc)
-    nodes = snap_nodes(line, nodes_fc)
-    nodes = sort_nodes(line, nodes.copy())
+    line = Line(edges_fc, nodes_fc)
 
-    network = build_graph(line, nodes)
-    pos = dict([(node['properties']['id'],list(node['geometry'].coords)[0]) for node in nodes])
+    network = line.graph()
+
+    pos = dict([(node['properties']['id'],list(node['geometry'].coords)[0]) for node in line.nodes])
     networkx.draw(network,pos,with_labels=True)
 
     fig,ax = plt.subplots(1,1,sharex=True,sharey=True)
 
-    gpd.GeoDataFrame(geometry=[line]).plot(ax=ax)
-    geometries = [node['geometry'] for node in nodes]
+    gpd.GeoDataFrame(geometry=[line.route]).plot(ax=ax)
+    geometries = [node['geometry'] for node in line.nodes]
     gpd.GeoDataFrame(geometry=geometries).plot(ax=ax, color='red')
 
-    for i in range(len(nodes)):
-        coords = list(nodes[i]['geometry'].coords)[0]
+    for i in range(len(line.nodes)):
+        coords = list(line.nodes[i]['geometry'].coords)[0]
         plt.annotate(str(i), xy=coords)
 
     plt.show()
